7314efe1919446f0c9e5a9dcc6aa61466fd44795
awglyde <awglyde@gmail.com>
2019-11-19 18:57:57 -0500

    Add initial file

--- game.py
+++ game.py
@@ -1,4 +1,10 @@
-import sys,pygame
+import pygame as pygrame
+import sys
+pygrame.init()
 
-while 1:
-    print("put yo code here")
+screen = pygrame.display.set_mode((500, 400))
+
+while True:
+    for event in pygrame.event.get():
+        if event.type is pygrame.QUIT:
+            sys.exit()
