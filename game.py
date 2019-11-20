f0ca6c480ac7eebba21c13c328e0531829c0dc5c
awglyde <awglyde@gmail.com>
2019-11-19 19:35:30 -0500

    Add each fighter to the screen

--- game.py
+++ game.py
@@ -3,6 +3,14 @@ import pygame as pygrame
 import sys
 pygrame.init()
 
+class Fighter:
+    def __init__(self, color, rect):
+        self.color = color
+        self.rect = rect
+
+    def draw(self, surface):
+        pygrame.draw.rect(surface, self.color, self.rect)
+
 class Ground:
     def __init__(self, color, rect):
         self.color = color
@@ -14,10 +22,15 @@ class Ground:
 def game_loop():
     screen = pygrame.display.set_mode((500, 400))
     ground = Ground(pygrame.Color(228, 59, 59), pygrame.Rect(50, 250, 400, 100))
+    fighter1 = Fighter(pygrame.Color(81, 72, 208), pygrame.Rect(60, 210, 25, 40))
+    fighter2 = Fighter(pygrame.Color(48, 139, 57), pygrame.Rect(400, 210, 25, 40))
     while True:
         for event in pygrame.event.get():
             if event.type is pygrame.QUIT:
                 sys.exit()
+        
+        fighter1.draw(screen)
+        fighter2.draw(screen)
         ground.draw(screen)
         pygrame.display.flip()
 
