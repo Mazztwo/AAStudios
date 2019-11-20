d676cb967769c08e0ca546862f326f52e9e69122
awglyde <awglyde@gmail.com>
2019-11-19 19:02:50 -0500

    Initialize screen at the start of the game loop

--- game.py
+++ game.py
@@ -2,9 +2,8 @@ import pygame as pygrame
 import sys
 pygrame.init()
 
-screen = pygrame.display.set_mode((500, 400))
-
 def game_loop():
+    screen = pygrame.display.set_mode((500, 400))
     while True:
         for event in pygrame.event.get():
             if event.type is pygrame.QUIT:
